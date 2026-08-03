7bd18e4d1760c0f180a4277b2d1ab59a30bd0921
Manas <manas.acharyya.nits.cse18@gmail.com>
2026-08-03 19:44:48 +0530

    Added more credits for Reddit Ads

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -26,6 +26,30 @@ const WORKOUT_VIDEOS = [
   {
     creator: "Gustavo Fring",
     href: "https://www.pexels.com/video/a-woman-exercising-using-dumbbells-6286152/"
+  },
+  {
+    creator: "Kampus Production",
+    href: "https://www.pexels.com/video/elderly-people-working-out-6023230/"
+  },
+  {
+    creator: "Instituto Alpha Fitness",
+    href: "https://www.pexels.com/video/energetic-woman-exercising-with-dumbbells-in-gym-37609128/"
+  },
+  {
+    creator: "Tima Miroshnichenko",
+    href: "https://www.pexels.com/video/a-woman-lifting-weights-6388424/"
+  },
+  {
+    creator: "Amar Preciado",
+    href: "https://www.pexels.com/video/woman-working-out-with-dumbbels-at-gym-11121740/"
+  },
+  {
+    creator: "Anastasia Shuraeva",
+    href: "https://www.pexels.com/video/couple-lifting-a-barbells-4944398/"
+  },
+  {
+    creator: "Pavel Danilyuk",
+    href: "https://www.pexels.com/video/woman-and-man-exercising-with-dumbbells-6326951/"
   }
 ] as const;
 
@@ -87,6 +111,54 @@ const ROOM_PHOTOS = [
   {
     creator: "Cup of Couple",
     href: "https://www.pexels.com/photo/a-man-and-woman-sitting-on-a-grassland-6962536/"
+  },
+  {
+    creator: "Andrea Piacquadio",
+    href: "https://www.pexels.com/photo/people-holding-a-weights-3766211/"
+  },
+  {
+    creator: "Ali Alcántara",
+    href: "https://www.pexels.com/photo/man-exercising-shirtless-14591553/"
+  },
+  {
+    creator: "Wendell Stoyer",
+    href: "https://www.pexels.com/photo/woman-preparing-for-workout-in-gym-environment-35419768/"
+  },
+  {
+    creator: "Tima Miroshnichenko",
+    href: "https://www.pexels.com/photo/a-man-and-woman-in-black-tank-top-working-out-inside-the-gym-6388367/"
+  },
+  {
+    creator: "Andrea Piacquadio",
+    href: "https://www.pexels.com/photo/three-women-s-doing-exercises-863977/"
+  },
+  {
+    creator: "Airam Dato-on",
+    href: "https://www.pexels.com/photo/woman-pushing-a-gym-equipment-13106608/"
+  },
+  {
+    creator: "Airam Dato-on",
+    href: "https://www.pexels.com/photo/a-man-and-an-woman-standing-beside-weight-plates-13106609/"
+  },
+  {
+    creator: "Tnarg",
+    href: "https://www.pexels.com/photo/woman-working-out-at-gym-5132095/"
+  },
+  {
+    creator: "Viridiana Rivera",
+    href: "https://www.pexels.com/photo/man-and-woman-exercising-together-14591736/"
+  },
+  {
+    creator: "Pavel Danilyuk",
+    href: "https://www.pexels.com/photo/people-stretching-using-dumbbells-6339480/"
+  },
+  {
+    creator: "Kampus Production",
+    href: "https://www.pexels.com/photo/man-training-on-gym-with-barbell-6922164/"
+  },
+  {
+    creator: "Airam Dato-on",
+    href: "https://www.pexels.com/photo/a-woman-in-sports-bra-and-black-shorts-is-sitting-on-gym-equipment-13106579/"
   }
 ] as const;
 
@@ -151,7 +223,7 @@ function PhotoCreditList({
 
 export default function CreditsPage() {
   return (
-    <LegalPage path="/credits" title="Credits" updated="July 31, 2026">
+    <LegalPage path="/credits" title="Credits" updated="August 3, 2026">
       <p>
         Some landing videos, room photos, and UI icons on RhoQ come from
         third-party creators. We thank them for their work.
